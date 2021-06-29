b7d621a29293039e29f55769806552f866eb29ae
Elias Peeters <Elias.peeters@otto.de>
2021-06-29 14:11:36 +0200

    Added first test

--- webpush.js
+++ webpush.js
@@ -1,1 +1,15 @@
 
+var push = require('web-push')
+
+let vapidKeys = {
+    publicKey: 'BOuyEHeM51woSIqB7PG5-EXGoPvSiSx0OuGN3MqkrLtVXSclej93_d4H9XuYPQ-cz9BEkZIR9f8gNWe-hxJEmkA',
+    privateKey: 'Ftg3RIGQYVTX8BFZ4SJwjPdHsb3riPWyeMYUvshgQ88'
+  }
+
+push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
+
+let sub = {
+    
+}
+
+push.sendNotification(sub, 'test message')
